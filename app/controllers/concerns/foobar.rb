66e3321be925bed3032e992974ee05a5d7baf465
Emily <emilylin502@gmail.com>
2015-04-30 00:18:17 -0700

    hw2 done

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,2 +1,13 @@
 class Foobar
+
+
+	include Comparable
+	include Enumerable
+	
+	def self.baz(arr)
+
+		return arr.uniq.inject(0) {|sum, i| (Integer(i) < 10 and Integer(i).even?) ? sum + Integer(i) + 2 : sum + 0}
+	end
+
+
 end
